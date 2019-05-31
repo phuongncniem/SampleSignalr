dd0b1f4b0c2dc62554326b371503f97f382b4857
phuongnc <phuongncniem@gmail.com>
2019-05-31 18:20:54 +0700

    Commit basic chat app

---
+++ SignalrChat/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalrChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public void SendP(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void SendPrivated(string name, string message, string connId)
+        {
+            Clients.Client(connId).appendNewMessage(name, message);
+        }
+
+        public void SendGroup(string name, string message, string connId)
+        {
+            Clients.Group(connId).appendNewMessage(name, message);
+        }
+    }
+}
---
+++ SignalrChat/ReadMe.cs
@@ -0,0 +1,3 @@
+﻿/*
+ * https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/mapping-users-to-connections#groups
+ * */
